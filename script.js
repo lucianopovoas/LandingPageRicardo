12d7114be2657c4b3d8e0173924d2b364c39937b
lucianopovoas <paixaoluciano69@gmail.com>
2025-05-16 11:45:07 -0300

    Identado

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ function nextSlide() {
 
 showSlide(index);
 setInterval(nextSlide, 4000); // troca a cada 4 segundos
+
+const swiper = new Swiper('.swiper', {
+      loop: true,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      slidesPerView: 3, // Mostra 3 slides ao mesmo tempo
+      spaceBetween: 30, // Espaço entre os slides (px)
+      breakpoints: {
+        0: {
+          slidesPerView: 1, // em telas pequenas
+        },
+        768: {
+          slidesPerView: 2, // tablets
+        },
+        1024: {
+          slidesPerView: 3, // desktops
+        }
+      }
+    });
